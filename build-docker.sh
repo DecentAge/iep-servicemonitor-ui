caef520d5fe66d26af8e9ba02c9624e0d5d617be
mazila <mazila@mazila-xps159560>
2021-06-01 16:06:01 +0200

    update

--- build-docker.sh
+++ build-docker.sh
@@ -7,7 +7,7 @@ RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-servicemonitor-ui:${RELEASE_VERSION} .
 
 
-CONTAINER_ID=$(docker create --rm --name iep-servicemonitor-ui-extr decentage/iep-servicemonitor-ui:${RELEASE_VERSION})
+CONTAINER_ID=$(docker create --rm --name iep-servicemonitor-ui-extrac decentage/iep-servicemonitor-ui:${RELEASE_VERSION})
 mkdir -p ./build
 docker cp ${CONTAINER_ID}:/build/iep-servicemonitor-ui.zip ./build
 docker rm ${CONTAINER_ID}
